ec373480a1bb2c88785ff2320e0716df85bf30b2
Caleb Westra <calebwestra2002@gmail.com>
2024-12-10 21:15:51 -0600

    added zombie patrolling

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
-from random import choice
+from random import choice, randint
 from math import sqrt
-from time import sleep
 
 root = Tk()
 
@@ -22,7 +21,6 @@ class Window:
 
 window = Window(800,1200,'gray')
 
-
 class Node:
     def __init__(self, start_x, start_y, diameter, color, speed):
         # attributes
@@ -41,6 +39,19 @@ class Node:
         window.game.coords(self.shape,self.x1,self.y1,self.x2,self.y2)
     def distance(self, other):
         return sqrt((self.center_x - other.center_x)**2 + (self.center_y - other.center_y)**2)
+    def boundary_check(self):
+        if self.y1 < window.top_boundary:
+            self.y1 = window.top_boundary
+            self.y2 = window.top_boundary + self.diameter
+        elif self.y2 > window.height:
+            self.y1 = window.height - self.diameter
+            self.y2 = window.height
+        if self.x1 < window.left_boundary:
+            self.x1 = window.left_boundary
+            self.x2 = window.left_boundary + self.diameter
+        elif self.x2 > window.width:
+            self.x1 = window.width - self.diameter
+            self.x2 = window.width
 
 class Player(Node):
     def __init__(self, start_x, start_y, diameter, color, speed):
@@ -86,19 +97,7 @@ class Player(Node):
         elif 'd' in self.key_presses:
             self.x1 += self.speed
             self.x2 += self.speed
-        # keep movement in boundary
-        if self.y1 < window.top_boundary:
-            self.y1 = window.top_boundary
-            self.y2 = window.top_boundary + self.diameter
-        elif self.y2 > window.height:
-            self.y1 = window.height - self.diameter
-            self.y2 = window.height
-        if self.x1 < window.left_boundary:
-            self.x1 = window.left_boundary
-            self.x2 = window.left_boundary + self.diameter
-        elif self.x2 > window.width:
-            self.x1 = window.width - self.diameter
-            self.x2 = window.width
+        self.boundary_check()
         # update shape and set after
         self.center_x = (self.x2+self.x1)/2; self.center_y = (self.y2 + self.y1) / 2
         self.draw()
@@ -110,14 +109,32 @@ player = Player(window.midwidth, window.midheight, 30, 'bisque', 2)
 class Zombie(Node):
     def __init__(self, start_x, start_y, diameter, color, speed):
         super().__init__(start_x,start_y,diameter,color,speed)
+        self.waypoint = None # (x,y) | None
     def move(self):
         if self.distance(player) < 200:
+            self.waypoint = None
             self.persue()
+        else:
+            self.patrol()
         self.center_x = (self.x2+self.x1)/2; self.center_y = (self.y2 + self.y1) / 2
-        root.after(10,self.move)
         self.draw()
+        root.after(10,self.move)
     def patrol(self):
-        direction = choice(['W','A','S','D','WA','WD','SA','SD'])
+        if not self.waypoint: self.create_waypoint()
+        if self.center_x > self.waypoint[0]:
+            self.x1 -= self.speed
+            self.x2 -= self.speed
+        else:
+            self.x1 += self.speed
+            self.x2 += self.speed
+        if self.center_y > self.waypoint[1]:
+            self.y1 -= self.speed
+            self.y2 -= self.speed
+        else:
+            self.y1 += self.speed
+            self.y2 += self.speed
+        self.boundary_check()
+        self.check_waypoint()
     def persue(self):
         if self.center_x > player.center_x:
             self.x1 -= self.speed
@@ -131,12 +148,26 @@ class Zombie(Node):
         elif self.center_y < player.center_y:
             self.y1 += self.speed
             self.y2 += self.speed
+    def create_waypoint(self):
+        new_x, new_y = randint(-200,200) + self.center_x, randint(-200,200) + self.center_y
+        if new_x > window.width: new_x = window.width
+        elif new_x < window.left_boundary: new_x = window.left_boundary
+        if new_y > window.height: new_y = window.height
+        elif new_y < window.top_boundary: new_y = window.top_boundary
+        self.waypoint = (new_x,new_y)
+    def check_waypoint(self):
+        if abs(self.center_x - self.waypoint[0]) < 50 and abs(self.center_y - self.waypoint[1]) < 50:
+            self.waypoint = None
 
-
-zombie = Zombie(200,320,30,'green',.75)
-
-
+zombies = []
+zombie_moves = []
+def spawn(n_zombies):
+    for i in range(n_zombies):
+        x_start, y_start = randint(0, window.width), randint(0, window.height)
+        zombie = Zombie(x_start,y_start,30,'green',.50)
+        zombies.append(zombie)
+        zombie_moves.append(root.after(10,zombie.move))
+spawn(4)
 
 root.after(10,player.move)
-root.after(10,zombie.move)
 root.mainloop()
